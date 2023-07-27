6db1caf311c2d855bf157cde4f6c0e6dd8e80c23
Bipashakh1267 <bipasha12345khandelwal@gmail.com>
2023-07-27 20:32:26 +0000

    Add files

---
+++ Frontend/src/components/PrivateRoute.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Home from '../pages/home';
+
+const PrivateRoute = () => {
+  const isLogged = localStorage.getItem('app-token');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isLogged && isLogged !== 'true') {
+      navigate('/login');
+    }
+  }, [isLogged, navigate]); 
+
+  return isLogged ? <Home /> : <></>;
+};
+
+export default PrivateRoute;
+
